2e96b97683940692a97f441c39db364bd98e5844
jchuharski <76936794+jchuharski@users.noreply.github.com>
2022-11-29 19:14:00 -0500

    zim

--- test/playground.jl
+++ test/playground.jl
@@ -1,1 +1,17 @@
-
+# city = read_city()
+# G = DiGraphFormat(read_city())
+# ans = Graphs.non_backtracking_randomwalk(G, 1, 2^40)
+# print(length(ans))
+# (; total_duration, nb_cars, starting_junction, streets) = city
+# itineraries = Vector{Vector{Int}}(undef, nb_cars)
+# for c in 1:nb_cars
+#     itineraries[c] = ans
+# end
+# sol = Solution(itineraries)
+# plot_streets(city, sol, path = "map.html")
+city = read_city()
+sol = greedy_sol(read_city())
+# HashCode2014.is_feasible(sol, city)
+print(HashCode2014.is_feasible(sol, city))
+# plot_streets(city, sol, path = "mmap.html")
+# HashCode2014.write_solution(sol, "feasible_sol_basic.txt")
