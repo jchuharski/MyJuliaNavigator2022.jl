9e6b9f3a03844f9446951c6f2aa0c7735edf40fe
jchuharski <76936794+jchuharski@users.noreply.github.com>
2023-01-21 13:35:33 -0500

    Update playground.jl

--- test/playground.jl
+++ test/playground.jl
@@ -1,20 +1,20 @@
 
 # using HashCode2014
-# using MyJuliaPackage
+# using MyJuliaNavigator2022
 
 # city = HashCode2014.read_city()
-# sol = MyJuliaPackage.gred(city; depth=10)
+# sol = MyJuliaNavigator2022.gred(city; depth=10)
 # print(HashCode2014.is_feasible(sol, city))
 # print(HashCode2014.total_distance(sol, city))
 
-# @code_warntype MyJuliaPackage.total_distance(sol.itineraries, graph)
-# @code_warntype MyJuliaPackage.gred(city)
+# @code_warntype MyJuliaNavigator2022.total_distance(sol.itineraries, graph)
+# @code_warntype MyJuliaNavigator2022.gred(city)
 # graph = create_graph(city)
 # seen = Set{Set{Int64}}()
 # current_junction = 4517
 # neighbors = graph[current_junction]
 # depth = 2
-# @code_warntype MyJuliaPackage.explorer_nd(neighbors, seen, current_junction, graph, depth)
+# @code_warntype MyJuliaNavigator2022.explorer_nd(neighbors, seen, current_junction, graph, depth)
 
 # #OUTPUT SOLN#
 # HashCode2014.plot_streets(city, sol; path="map1864255.html")
@@ -22,6 +22,6 @@
 
 # # bound
 
-# infeasible = MyJuliaPackage.bidirectional_graph(city)
+# infeasible = MyJuliaNavigator2022.bidirectional_graph(city)
 # upper_bound_distance(city; short=true)
 # upper_bound_speed(city; short=true)
