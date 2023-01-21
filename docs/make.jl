d3770c5881481194b569c5d63a935a65b21c611d
jchuharski <76936794+jchuharski@users.noreply.github.com>
2023-01-21 13:32:40 -0500

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,20 +1,20 @@
-using MyJuliaPackage
+using MyJuliaNavigator2022
 using Documenter
 
-DocMeta.setdocmeta!(MyJuliaPackage, :DocTestSetup, :(using MyJuliaPackage); recursive=true)
+DocMeta.setdocmeta!(MyJuliaNavigator2022, :DocTestSetup, :(using MyJuliaPackage); recursive=true)
 
 makedocs(;
-    modules=[MyJuliaPackage],
+    modules=[MyJuliaNavigator2022],
     authors="jchuharski <76936794+jchuharski@users.noreply.github.com> and contributors",
-    repo="https://github.com/jchuharski/MyJuliaPackage.jl/blob/{commit}{path}#{line}",
-    sitename="MyJuliaPackage.jl",
+    repo="https://github.com/jchuharski/MyJuliaNavigator2022.jl/blob/{commit}{path}#{line}",
+    sitename="MyJuliaNavigator2022.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://jchuharski.github.io/MyJuliaPackage.jl",
+        canonical="https://jchuharski.github.io/MyJuliaNavigator2022.jl",
         edit_link="main",
         assets=String[],
     ),
     pages=["Home" => "index.md", "Algorithm Docs" => "Algorithm.md"],
 )
 
-deploydocs(; repo="github.com/jchuharski/MyJuliaPackage.jl", devbranch="main")
+deploydocs(; repo="github.com/jchuharski/MyJuliaNavigator2022.jl", devbranch="main")
